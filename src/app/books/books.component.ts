bdfd9a66ae39b12f88cd945ff7a9a5b1a9176fc4
Daniel <danielngoys@outlook.com>
2023-02-08 09:23:57 +0300

    ✨ useful variables

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent {
+  name:string = 'Clean Code';
+  author:string = 'Robert C Martin';
+  image:string = "https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg";
+
+
+  isDisabled:boolean = false
+
+  handleClick(){
+    this.isDisabled = true
+    alert('yeepy')
+  }
 
 }
