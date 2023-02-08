058a465cde19eddd85a3e694a391a705699493d2
Daniel <danielngoys@outlook.com>
2023-02-08 14:56:17 +0300

    ✨ data transfer between parent and child

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from '../interface/Book';
 
 @Component({
   selector: 'app-books',
@@ -6,10 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent {
-  name:string = 'Clean Code';
-  author:string = 'Robert C Martin';
-  image:string = "https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg";
+
+  books:Book[] =[
+    {
+      name: 'Clean Code',
+      author: 'Robert C Martin',
+      image: "https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      amount: 41.86
+    },
+    {
+      name: 'The Pragmatic Programmer',
+      author: ' David Thomas ',
+      image: "https://m.media-amazon.com/images/I/51IA4hT6jrL._SX380_BO1,204,203,200_.jpg",
+      amount: 39
+    }
+  ]
+
+  cart:Book[]= [];
 
   iShowing:boolean = true
+
+  addToCart(bookng: Book){
+    console.log(bookng)
+  }
 
 }
