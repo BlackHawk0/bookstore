a2fcf33a23f6646da953cbaa21d01e726d7eb998
Daniel <danielngoys@outlook.com>
2023-02-08 17:40:35 +0300

    ✨ get a service

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Book } from '../interface/Book';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-books',
@@ -7,25 +8,17 @@ import { Book } from '../interface/Book';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent {
+  books: Book[] = [];
 
-  books:Book[] =[
-    {
-      name: 'Clean Code',
-      author: 'Robert C Martin',
-      image: "https://m.media-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-      amount: 41.86
-    },
-    {
-      name: 'The Pragmatic Programmer',
-      author: ' David Thomas ',
-      image: "https://m.media-amazon.com/images/I/51IA4hT6jrL._SX380_BO1,204,203,200_.jpg",
-      amount: 39
-    }
-  ]
+  constructor(private booksService: BookService){ }
+  ngOnInit(): void {
+    this.books = this.booksService.getBooks()
+  }
 
-  cart:Book[]= [];
 
-  iShowing:boolean = true
+  cart: Book[] = [];
+
+  iShowing: boolean = true
 
   addToCart(bookng: Book){
     console.log(bookng)
