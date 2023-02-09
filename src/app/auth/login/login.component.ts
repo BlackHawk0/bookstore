f042d4ebade2321ccd163c44e6385c1d587c7d72
Daniel <danielngoys@outlook.com>
2023-02-09 12:50:28 +0300

    🎨 mplement two way binding (formsmodule)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginForm } from 'src/app/interface/auth';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  form: LoginForm = {
+    email: '',
+    password: ''
+  }
+
+  submit(){
+    alert(this.form.email)
+  }
 
 }
