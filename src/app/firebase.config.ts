cc4041c16981684b8611120803371de89137be48
Daniel <danielngoys@outlook.com>
2023-02-10 08:53:13 +0300

    🔧: firebase configuration

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyWIbn4pTZxcrFA1x3NZN93rEtXh1f930",
+  authDomain: "bookstore-d1aee.firebaseapp.com",
+  projectId: "bookstore-d1aee",
+  storageBucket: "bookstore-d1aee.appspot.com",
+  messagingSenderId: "159604756082",
+  appId: "1:159604756082:web:dff1f0d6a13d1b58c4fceb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
