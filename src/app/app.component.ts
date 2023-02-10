3312e15ca767d570b6ce689560f35416fe9625d2
Daniel <danielngoys@outlook.com>
2023-02-10 08:54:36 +0300

    🎨 firebase intialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
+import { app } from "./firebase.config";
 
 @Component({
   selector: 'app-root',
   templateUrl: "./app.component.html",
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{}
+export class AppComponent{
+
+  ngOnInit(): void {
+    app
+  }
+}
