dd40e0a1aba92f089a292774660a619498e8571b
Daniel <danielngoys@outlook.com>
2023-02-13 12:23:50 +0300

    ⚡️ refactored code to improve performance

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { AppComponent } from '../app.component';
 import { LoginForm, RegisterForm } from '../interface/auth';
 
 @Injectable({
@@ -9,49 +8,68 @@ import { LoginForm, RegisterForm } from '../interface/auth';
 })
 export class AuthService {
   isAuthenticated: boolean = false
+  isLoading: boolean = false;
+  passwordMatched: boolean = false
+
   constructor(private router: Router) { }
 
   // register
   register(form: RegisterForm){
+
+    if (this.isLoading) return;
+
+    this.isLoading = true;
+
+    if (form.password !== form.confirm_password) {
+      this.passwordMatched = false;
+      return;
+    }
+
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, form.email, form.password)
     .then((userCredential) => {
-      // Signed in
-
       this.router.navigate(['login'])
       const user = userCredential.user;
 
-      // ...
     })
     .catch((error) => {
+      this.isAuthenticated = false
       const errorCode = error.code;
       const errorMessage = error.message;
-    });
+    })
+    .finally(()=>{this.isLoading = false})
   }
 
   // login
   login(form: LoginForm){
+    if (this.isLoading) return;
+
+    this.isLoading = true;
+
     const auth = getAuth();
     signInWithEmailAndPassword(auth, form.email, form.password)
     .then((userCredential) => {
-      // Signed in
       this.isAuthenticated = true
       this.router.navigate([''])
-      // ...
+
     })
     .catch((error) => {
+      this.isAuthenticated = false
       const errorCode = error.code;
       const errorMessage = error.message;
     })
+    .finally(() => (this.isLoading = false));
 
   }
 
   logout(){
     const auth = getAuth();
     signOut(auth).then(() => {
-      // Sign-out successful.
+      this.router.navigate(['login']);
+      this.isAuthenticated = false;
     }).catch((error) => {
-      // An error happened.
+      console.log(error);
+
     });
   }
 
