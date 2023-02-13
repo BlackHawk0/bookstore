d6bc026739f00b57e77aaba1098ac21c71a9dc7c
Daniel <danielngoys@outlook.com>
2023-02-13 11:15:08 +0300

    ✨ implemented logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,43 +1,57 @@
 import { Injectable } from '@angular/core';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { Router } from '@angular/router';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { AppComponent } from '../app.component';
 import { LoginForm, RegisterForm } from '../interface/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  isAuthenticated: boolean = false
+  constructor(private router: Router) { }
 
-  constructor() { }
-
+  // register
   register(form: RegisterForm){
     const auth = getAuth();
-    signInWithEmailAndPassword(auth, form.email, form.password)
+    createUserWithEmailAndPassword(auth, form.email, form.password)
     .then((userCredential) => {
       // Signed in
+
+      this.router.navigate(['login'])
       const user = userCredential.user;
-      alert('user is logged in')
+
       // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      alert("Incorrect details")
     });
   }
 
+  // login
   login(form: LoginForm){
     const auth = getAuth();
     signInWithEmailAndPassword(auth, form.email, form.password)
     .then((userCredential) => {
       // Signed in
-      const user = userCredential.user;
-      alert('user is logged in')
+      this.isAuthenticated = true
+      this.router.navigate([''])
       // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      alert("Incorrect details")
+    })
+
+  }
+
+  logout(){
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
     });
   }
 
